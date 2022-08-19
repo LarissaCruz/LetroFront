b01e40f5a89b28051b072dfc65ece30bff894721
LarissaCruz <61520705+LarissaCruz@users.noreply.github.com>
2022-08-19 11:32:52 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
 import { useFonts } from "expo-font";
-import MainStackNavigator from "./src/routes/MainStackNavigator";
-import AuthStackNavigator from "./src/routes/AuthStackNavigator";
 import { useAuth } from "./src/hooks/useAuth";
 import { UserContext } from "./src/context/UserContext";
 import { AuthContext } from "./src/context/AuthContext";
+import MainStackNavigator from "./src/routes/MainStackNavigator";
+import AuthStackNavigator from "./src/routes/AuthStackNavigator";
 
 export default function App() {
   const [statusFont, setStatusFont] = useState(false);
   const { auth, state } = useAuth();
-  
   let [fontsLoaded] = useFonts({
     "Avenir-Bold": require("./assets/fonts/AvenirLTStd-Black.otf"),
     "Avenir-Regular": require("./assets/fonts/AvenirLTStd-Book.otf"),
@@ -22,11 +21,7 @@ export default function App() {
     }
   }, [fontsLoaded]);
 
-  function renderScreens() {
-    // if (state.loading) {
-    // <RootStack.Screen name={'Splash'} component={SplashScreen} />;
-    // return null;
-    //}
+  const renderScreens = () => {
     return state.user ? (
       <UserContext.Provider value={state.user}>
         <MainStackNavigator />
@@ -34,18 +29,16 @@ export default function App() {
     ) : (
       <AuthStackNavigator />
     );
-  }
+  };
 
   return (
-    <>
-      {!!statusFont && (
-        <>
-          <StatusBar backgroundColor={"#252837"}></StatusBar>
-          <AuthContext.Provider value={auth}>
-            {renderScreens()}
-          </AuthContext.Provider>
-        </>
-      )}
-    </>
+    !!statusFont && (
+      <>
+        <StatusBar backgroundColor={"#252837"}></StatusBar>
+        <AuthContext.Provider value={auth}>
+          {renderScreens()}
+        </AuthContext.Provider>
+      </>
+    )
   );
 }
